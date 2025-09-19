a859646f7f320e35ea09464564a636b753df30d9
Dmytro Yaroshenko <73843436+o-murphy@users.noreply.github.com>
2025-09-19 12:42:07 +0300

    tester for stm dongle

--- stmtest/code.py
+++ stmtest/code.py
@@ -4,28 +4,39 @@ import board
 import busio
 import digitalio
 
-from lrfemu import random_range, request_pack
+from lrfemu_min import random_range, request_pack
 
-# Pin definitions for pins 6 and 7
-# The RP2040 Zero board labels these as GP6 and GP7
-led6_red = digitalio.DigitalInOut(board.GP21)
-led7_green = digitalio.DigitalInOut(board.GP22)
+# defines
+# led pins
+LED_RED_PIN = board.GP21
+LED_GREEN_PIN = board.GP22
+LED_ACTIVE_HIGH = True
 
-# Set the pins as outputs
-led6_red.direction = digitalio.Direction.OUTPUT
-led7_green.direction = digitalio.Direction.OUTPUT
+# btn pin
+BTN_PIN = board.GP16
 
-btn_pin = digitalio.DigitalInOut(board.GP16)
-btn_pin.direction = digitalio.Direction.INPUT
-btn_pin.pull = digitalio.Pull.UP
+# uart pins
+UART_0_TX_PIN = board.GP4
+UART_0_RX_PIN = board.GP5
+UART_1_TX_PIN = board.GP12
+UART_1_RX_PIN = board.GP13
 
-# Create uart_core with transmit (TX) and receive (RX) pins.
-# For Raspberry Pi Pico, TX is GP0 and RX is GP1.
-uart_core = busio.UART(board.GP4, board.GP5, baudrate=115200, timeout=1.0)
+# packet pattern
+CORE_TO_LRF_PACKET = b'\xee\x16\x04\x03\xa1\n\x00\xae'
 
-# Create uart_lrf with transmit (TX) and receive (RX) pins.
-# For Raspberry Pi Pico, TX is GP4 and RX is GP5.
-uart_lrf = busio.UART(board.GP12, board.GP13, baudrate=115200, timeout=1.0)
+
+class UniLED:
+    def __init__(self, pin, active_high=True):
+        self._pin = digitalio.DigitalInOut(pin)
+        self._pin.direction = digitalio.Direction.OUTPUT
+        self._active_high = active_high
+        self.off()
+
+    def on(self):
+        self._pin.value = self._active_high
+
+    def off(self):
+        self._pin.value = not self._active_high
 
 
 def format_bytes_as_hex(data):
@@ -33,62 +44,73 @@ def format_bytes_as_hex(data):
     return " ".join([f"{b:02X}" for b in data])
 
 
-def core_to_lrf():
-    # CORE_TO_LRF:
+def loopback(uart_a, uart_b, data: bytes, timeout=0.5):
+    uart_a.write(data)
+    time.sleep(timeout)
+    if uart_b.in_waiting > 0:
+        received_data = uart_b.read(uart_b.in_waiting)
+    else:
+        received_data = b''
+    received_data = received_data.strip().strip(b'\x00')
+    return received_data
+
+
+def core_to_lrf(uart_core, uart_lrf):
+    "CORE_TO_LRF:"
     # packet = b'\xee' + bytes([random.randint(0, 255) for i in range(10)])
-    packet = b'\xee\x16\x04\x03\xa1\n\x00\xae'
-    print(f"uart_core sending: {format_bytes_as_hex(packet)}")
-    uart_core.write(packet)  # Encode the string to bytes
-
-    time.sleep(0.5)  # Small delay to allow data to be received
-
-    # Check if there's any data available on uart_lrf
-    if uart_lrf.in_waiting > 0:
-        received_data = uart_lrf.read(uart_lrf.in_waiting)
-    else:
-        received_data = b''
-    print(f"uart_lrf received: {format_bytes_as_hex(received_data)}")
-    ret = received_data.strip().strip(b'\x00') == packet
-    print("CORE_TO_LRF:", ret)
-    print()
-    return ret
+    print(f"uart_core sending: {format_bytes_as_hex(CORE_TO_LRF_PACKET)}")
+    received_data = loopback(uart_core, uart_lrf, CORE_TO_LRF_PACKET)
+    state = received_data == CORE_TO_LRF_PACKET
+    print(f"uart_lrf received: {format_bytes_as_hex(received_data)}, {state=}\n")
+    return state
 
 
-def lrf_to_core():
-    # LRF_TO_CORE:
+def lrf_to_core(uart_lrf, uart_core):
+    "LRF_TO_CORE:"
     _range = random_range()
-    _expected = str(int(_range // 1)).encode('ansi')
-    packet = bytes(request_pack(0x02, status=0, range=_range))
+    expected = str(int(_range // 1)).encode('ansi')
+    packet = bytes(request_pack(0x02, status=0, range_=_range))
     print(f"uart_lrf sending:  {format_bytes_as_hex(packet)} ({_range})")
+    received_data = loopback(uart_lrf, uart_core, packet)
+    state = received_data == expected
+    print(f"uart_core received: {received_data}, expected: {expected}, {state=}\n")
+    return state
 
-    uart_lrf.write(packet)  # Encode the string to bytes
 
-    time.sleep(0.5)  # Small delay to allow data to be received
-    if uart_core.in_waiting > 0:
-        received_data = uart_core.read(uart_core.in_waiting)
-    else:
-        received_data = b''
-    print(f"uart_core received: {received_data}, expected: {_expected}")
-    ret = received_data.strip().strip(b'\x00') == _expected
-    print("LRF_TO_CORE:", ret)
-    print()
-    return ret
+def check(uart_core, uart_lrf):
+    led_red.off()
+    led_green.off()
 
+    time.sleep(0.5)
+
+    status0 = core_to_lrf(uart_core, uart_lrf)
+    time.sleep(0.1)
+    status1 = lrf_to_core(uart_lrf, uart_core)
+    status = status0 and status1
+
+    led_green.on() if status else led_red.on()
+
+
+# init pins
+btn_pin = digitalio.DigitalInOut(BTN_PIN)
+btn_pin.direction = digitalio.Direction.INPUT
+btn_pin.pull = digitalio.Pull.UP
+
+# Pin definitions for pins 6 and 7
+# The RP2040 Zero board labels these as GP6 and GP7
+led_red = UniLED(LED_RED_PIN, LED_ACTIVE_HIGH)
+led_green = UniLED(LED_GREEN_PIN, LED_ACTIVE_HIGH)
+
+# Create uart_core with transmit (TX) and receive (RX) pins.
+# For Raspberry Pi Pico, TX is GP0 and RX is GP1.
+uart_0 = busio.UART(UART_0_TX_PIN, UART_0_RX_PIN, baudrate=115200, timeout=1.0)  # Core side
+
+# Create uart_lrf with transmit (TX) and receive (RX) pins.
+# For Raspberry Pi Pico, TX is GP4 and RX is GP5.
+uart_1 = busio.UART(UART_1_TX_PIN, UART_1_RX_PIN, baudrate=115200, timeout=1.0)  # LRF side
 
 if __name__ == "__main__":
     print("Starting UART loopback test...")
     while True:
         if btn_pin.value == False:
-            led6_red.value = False
-            led7_green.value = False
-
-            # status = core_to_lrf() and lrf_to_core()
-            status0 = core_to_lrf()
-            time.sleep(0.1)
-            status1 = lrf_to_core()
-
-            status = status0 and status1
-
-            led7_green.value, led6_red.value = status, not status
-
-            time.sleep(0.2)  # Wait for 2 seconds before the next message
+            check(uart_0, uart_1)
